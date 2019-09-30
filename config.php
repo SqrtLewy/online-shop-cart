f13192cfd5d59a6a202136a368db625f0f6be9b3
Arkadiusz Lewicki <43863162+SqrtLewy@users.noreply.github.com>
2019-09-30 10:46:04 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 function pdo_connect_mysql() {
-    $DATABASE_HOST = 'localhost';
-	 $DATABASE_NAME = 'shop';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
+	$DATABASE_HOST = 'localhost';
+	$DATABASE_NAME = 'shop';
+	$DATABASE_USER = 'root';
+	$DATABASE_PASS = '';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $e) {
