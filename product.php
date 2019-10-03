c7fe5edac35c52689f11f371fef27c9c451dc430
Arkadiusz Lewicki <43863162+SqrtLewy@users.noreply.github.com>
2019-10-03 12:01:23 +0200

    Update product.php

--- product.php
+++ product.php
@@ -4,6 +4,7 @@
 	<head>
 		<title>Online Shop</title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
 		
 		<?php
 		include 'config.php';
@@ -17,21 +18,19 @@
 		<div id="container">
 		
 			<div id="header">
-				<h2>Online Shop - Everyday low prices!</h2>
-				<?php
-					if (isset($_GET['id'])) {
-					$stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
-					$stmt->execute([$_GET['id']]);
-					$product = $stmt->fetch(PDO::FETCH_ASSOC);
-					if (!$product) {
-						die ('Product does not exist!');
-					}
-					} else {
-						die ('Product does not exist!');
-					}
-				?>
-
-				
+				<h2><a href="home.php">Online Shop</a> - Everyday low prices!</h2>
+					<?php
+						if (isset($_GET['id'])) {
+						$stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
+						$stmt->execute([$_GET['id']]);
+						$product = $stmt->fetch(PDO::FETCH_ASSOC);
+						if (!$product) {
+							die ('Product does not exist!');
+						}
+						} else {
+							die ('Product does not exist!');
+						}
+					?>
 			</div>
 			
 			<div id="article">
@@ -40,8 +39,9 @@
 			<div>
         		<h1 class="name"><?=$product['name']?></h1>
 				<span class="price">
-				&dollar;<?=$product['price']?>
+					&dollar;<?=$product['price']?>
 				</span>
+				
         <form action="index.php?page=cart" method="post">
             <input type="number" name="quantity" value="1" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
             <input type="hidden" name="product_id" value="<?=$product['id']?>">
