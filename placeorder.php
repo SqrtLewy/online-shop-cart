9e40dcfe42e8081201249774d44b6d9cfd57e98d
Arkadiusz Lewicki <43863162+SqrtLewy@users.noreply.github.com>
2019-10-03 12:04:07 +0200

    Update placeorder.php

--- placeorder.php
+++ placeorder.php
@@ -2,4 +2,5 @@
 <div id="order">
     <h1>Your order has been accepted!</h1>
     <p>Thank you for shopping! We will contact you via e-mail.</p>
+    <a href="home.php">Click</a> to return to the store.
 </div>
